fef12226916c5d3d1e73cebe594c18ad8d615c9d
Tilman Ihrig <ihrigtil@uni-bremen.de>
2020-03-22 23:33:29 +0100

    Standardize Google API placeholders

--- app/ackersnacker/ios/Runner/AppDelegate.swift
+++ app/ackersnacker/ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("PLACE_GOOGLE_API_KEY")
+    GMSServices.provideAPIKey("PLACE_GOOGLE_API_KEY_HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
